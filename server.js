327515507d8fc44bab58ff637a343be477ed5b26
ibanb <ibn144000@gmail.com>
2022-10-16 16:21:32 +0300

    add express

---
+++ server.js
@@ -0,0 +1,10 @@
+import Express from 'express';
+const app = new Express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
